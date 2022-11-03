2860ab7bd553572590de34d1b92886d05b7f0884
Kevin De Keyser <kevin@dekeyser.ch>
2022-11-03 12:16:09 +0100

    described build routine

--- fluttergcc11wayland/build.sh
+++ fluttergcc11wayland/build.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
+# Build the container.
 docker build -t fluttergcc11wayland .
-#docker volume create --name fluttergcc11waylandvolume -o size=100G 
-#docker run -it --storage-opt size=100G fluttergcc11wayland
+# only create mount if it does not exist. build will be removed anyways.
+if [ $(docker volume inspect fluttergcc11waylandvolume | wc -c) -lt "50" ] then
+  docker volume create --name fluttergcc11waylandvolume -o size=100G 
+fi
+# Set permissions
 docker run -it --rm -v fluttergcc11waylandvolume:/workdir fluttergcc11wayland sudo chown -R build:build /workdir
+# Install flutter wayland
 docker run -it -v fluttergcc11waylandvolume:/workdir fluttergcc11wayland /home/build/wayland.sh
+# Enter the container to check out the results
 docker run -it -v fluttergcc11waylandvolume:/workdir fluttergcc11wayland /bin/bash
 

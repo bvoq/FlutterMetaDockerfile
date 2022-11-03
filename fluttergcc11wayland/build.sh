934fa99cf082a23e8324a19cef9681b6eaaf7457
Kevin De Keyser <kevin@dekeyser.ch>
2022-11-03 12:52:49 +0100

    remove check

--- fluttergcc11wayland/build.sh
+++ fluttergcc11wayland/build.sh
@@ -2,10 +2,7 @@
 # Build the container.
 docker build -t fluttergcc11wayland .
 # only create mount if it does not exist. build will be removed anyways.
-if [ $(docker volume inspect fluttergcc11waylandvolume | wc -c) -lt "50" ]
-then
 docker volume create --name fluttergcc11waylandvolume -o size=100G 
-fi
 # Set permissions
 docker run -it --rm -v fluttergcc11waylandvolume:/workdir fluttergcc11wayland sudo chown -R build:build /workdir
 # Install flutter wayland

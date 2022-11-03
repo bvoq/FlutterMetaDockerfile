bb81a8ad5d459a29c5016a5515078efd146a5568
Kevin De Keyser <kevin@dekeyser.ch>
2022-11-03 23:56:11 +0100

    add deploy

--- fluttergcc11wayland/build.sh
+++ fluttergcc11wayland/build.sh
@@ -8,5 +8,8 @@ docker run -it --rm -v fluttergcc11waylandvolume:/workdir fluttergcc11wayland su
 # Install flutter wayland
 docker run -it -v fluttergcc11waylandvolume:/workdir fluttergcc11wayland /home/build/wayland.sh
 # Enter the container to check out the results
-docker run -it -v fluttergcc11waylandvolume:/workdir fluttergcc11wayland /bin/bash
+# docker run -it -v fluttergcc11waylandvolume:/workdir fluttergcc11wayland /bin/bash
 
+# After building result create a dummy container to copy away the volume storage.
+docker container create --name dummy -v fluttergcc11waylandvolume:/root hello-world
+docker cp dummy:/root/build/tmp/deploy deploy_wayland

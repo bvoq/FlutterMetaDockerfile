e68d352228f5d6cb96b40d1e96180bcc71b17967
Kevin De Keyser <kevin@dekeyser.ch>
2022-11-08 14:42:31 +0100

    added build arm

---
+++ fluttergcc11sdk/myproj/build2/elinux/arm64/release/flutter/CMakeFiles/flutter_assemble.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/flutter_assemble"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/flutter_assemble.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

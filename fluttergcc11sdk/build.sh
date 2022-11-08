2266f83e836973e4bdac11f93936335fa034e564
Kevin De Keyser <kevin@dekeyser.ch>
2022-11-08 13:42:03 +0100

    added sdk

--- fluttergcc11sdk/build.sh
+++ fluttergcc11sdk/build.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 docker build -t fluttergcc11sdk .
-docker volume create --name fluttergcc11sdkvolume -o size=100G 
+#docker volume create --name fluttergcc11sdkvolume -o size=100G 
 #docker run -it --storage-opt size=100G fluttergcc11wayland
 docker run -it --rm -v fluttergcc11sdkvolume:/workdir fluttergcc11sdk sudo chown -R build:build /workdir
 docker run -it -v fluttergcc11sdkvolume:/workdir fluttergcc11sdk /home/build/sdk.sh
 docker run -it -v fluttergcc11sdkvolume:/workdir fluttergcc11sdk /bin/bash
 
-docker container create --name dummysdk -v fluttergcc11sdkvolume:/root hello-world
-docker cp dummysdk:/root/build/tmp/deploy deploy_sdk
+docker container create --name dummyelinux -v fluttergcc11sdkvolume:/root hello-world
+docker cp dummyelinux:/root/build/tmp/deploy deploy_sdk
+docker cp dummyelinux:/root/myproj myproj

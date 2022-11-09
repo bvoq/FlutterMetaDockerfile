52bc1c251c496daf3d51de76bf472b2e4d726af2
Kevin De Keyser <kevin@dekeyser.ch>
2022-11-09 10:34:58 +0100

    added flgcc11

--- fluttergcc11sdk/step3_runonqemu64.sh
+++ fluttergcc11sdk/step3_runonqemu64.sh
@@ -8,4 +8,5 @@ brew install qemu
 # emerge --ask app-emulation/qemu # Gentoo
 # zypper install qemu # SUSE
 
-qemu-system-aarch64 -m 2048 -cpu cortex-a57 -smp 2 -M virt -bios QEMU_EFI.fd -serial stdio  -device virtio-net,netdev=hostnet0,mac=52:54:00:09:a4:37 -netdev user,id=hostnet0 -drive if=none,file=Fedora-Workstation-36-1.5.aarch64.raw,id=hd0  -device virtio-blk-device,drive=hd0 -device virtio-gpu-pci -device nec-usb-xhci -device usb-tablet -device usb-kbd
+# The 'mon:' in the -serial line fixes ctrl+C so it doesn't kill qemu;
+qemu-system-aarch64 -m 4096 -cpu cortex-a57 -smp 2 -M virt -bios QEMU_EFI.fd -serial mon:stdio  -device virtio-net,netdev=hostnet0,mac=52:54:00:09:a4:37 -netdev user,id=hostnet0 -drive if=none,file=Fedora-Workstation-36-1.5.aarch64.raw,id=hd0  -device virtio-blk-device,drive=hd0 -device virtio-gpu-pci -device nec-usb-xhci -device usb-tablet -device usb-kbd -hdb fat:./flutterapp

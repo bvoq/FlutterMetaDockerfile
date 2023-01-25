3240993dd12f967a4cfb4b0fe33428d10abf1a4e
Kevin De Keyser <kevin@dekeyser.ch>
2023-01-25 23:02:25 +0100

    example

---
+++ flutterembeddedlinux/build/elinux/arm64/release/flutter/CMakeFiles/flutter_wrapper_app.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/workdir/myproj/elinux/flutter/ephemeral/cpp_client_wrapper/core_implementations.cc" "flutter/CMakeFiles/flutter_wrapper_app.dir/ephemeral/cpp_client_wrapper/core_implementations.cc.o" "gcc" "flutter/CMakeFiles/flutter_wrapper_app.dir/ephemeral/cpp_client_wrapper/core_implementations.cc.o.d"
+  "/workdir/myproj/elinux/flutter/ephemeral/cpp_client_wrapper/flutter_engine.cc" "flutter/CMakeFiles/flutter_wrapper_app.dir/ephemeral/cpp_client_wrapper/flutter_engine.cc.o" "gcc" "flutter/CMakeFiles/flutter_wrapper_app.dir/ephemeral/cpp_client_wrapper/flutter_engine.cc.o.d"
+  "/workdir/myproj/elinux/flutter/ephemeral/cpp_client_wrapper/flutter_view_controller.cc" "flutter/CMakeFiles/flutter_wrapper_app.dir/ephemeral/cpp_client_wrapper/flutter_view_controller.cc.o" "gcc" "flutter/CMakeFiles/flutter_wrapper_app.dir/ephemeral/cpp_client_wrapper/flutter_view_controller.cc.o.d"
+  "/workdir/myproj/elinux/flutter/ephemeral/cpp_client_wrapper/standard_codec.cc" "flutter/CMakeFiles/flutter_wrapper_app.dir/ephemeral/cpp_client_wrapper/standard_codec.cc.o" "gcc" "flutter/CMakeFiles/flutter_wrapper_app.dir/ephemeral/cpp_client_wrapper/standard_codec.cc.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ flutterembeddedlinux/build/elinux/arm64/release/runner/CMakeFiles/myproj.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for myproj.
+# This may be replaced when dependencies are built.

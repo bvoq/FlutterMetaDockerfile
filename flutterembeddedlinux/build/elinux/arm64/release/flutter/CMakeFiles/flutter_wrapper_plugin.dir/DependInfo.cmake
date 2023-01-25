1270a04848664a31c638b7f0776563d7bf160945
Kevin De Keyser <kevin@dekeyser.ch>
2023-01-25 20:51:49 +0100

    build

---
+++ flutterembeddedlinux/build/elinux/arm64/release/flutter/CMakeFiles/flutter_wrapper_plugin.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/workdir/myproj/elinux/flutter/ephemeral/cpp_client_wrapper/core_implementations.cc" "flutter/CMakeFiles/flutter_wrapper_plugin.dir/ephemeral/cpp_client_wrapper/core_implementations.cc.o" "gcc" "flutter/CMakeFiles/flutter_wrapper_plugin.dir/ephemeral/cpp_client_wrapper/core_implementations.cc.o.d"
+  "/workdir/myproj/elinux/flutter/ephemeral/cpp_client_wrapper/plugin_registrar.cc" "flutter/CMakeFiles/flutter_wrapper_plugin.dir/ephemeral/cpp_client_wrapper/plugin_registrar.cc.o" "gcc" "flutter/CMakeFiles/flutter_wrapper_plugin.dir/ephemeral/cpp_client_wrapper/plugin_registrar.cc.o.d"
+  "/workdir/myproj/elinux/flutter/ephemeral/cpp_client_wrapper/standard_codec.cc" "flutter/CMakeFiles/flutter_wrapper_plugin.dir/ephemeral/cpp_client_wrapper/standard_codec.cc.o" "gcc" "flutter/CMakeFiles/flutter_wrapper_plugin.dir/ephemeral/cpp_client_wrapper/standard_codec.cc.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ flutterembeddedlinux/build/elinux/arm64/release/runner/CMakeFiles/myproj.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/workdir/myproj/elinux/flutter/generated_plugin_registrant.cc" "runner/CMakeFiles/myproj.dir/__/flutter/generated_plugin_registrant.cc.o" "gcc" "runner/CMakeFiles/myproj.dir/__/flutter/generated_plugin_registrant.cc.o.d"
+  "/workdir/myproj/elinux/runner/flutter_window.cc" "runner/CMakeFiles/myproj.dir/flutter_window.cc.o" "gcc" "runner/CMakeFiles/myproj.dir/flutter_window.cc.o.d"
+  "/workdir/myproj/elinux/runner/main.cc" "runner/CMakeFiles/myproj.dir/main.cc.o" "gcc" "runner/CMakeFiles/myproj.dir/main.cc.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/workdir/myproj/build/elinux/arm64/release/flutter/CMakeFiles/flutter_wrapper_app.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
